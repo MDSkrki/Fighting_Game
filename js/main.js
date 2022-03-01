299189a0c6cf8c735b2bacefdcff06f03f0c0d28
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-01 09:27:07 +0100

    added audio to screen change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+const audioPlay = () => {
+    let audio = document.getElementById("audio");
+    audio.play();
+}
+
 const hideStartScreen = () => {
     let startScreen = document.getElementById("startScreen");
     let selectionScreen = document.getElementById("selectionScreen")
     startScreen.style.display = 'none';
     selectionScreen.style.display = 'block';
+    audioPlay();
 }
