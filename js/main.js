9fcea1e8e6953372f5ce7901b55765c021bd105f
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-02 09:08:35 +0100

    rearranged inputs and characters buttons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const audioPlay = () => {
+// Start Screen
+const startAudioPlay = () => {
     let audio = document.getElementById("audio");
     audio.play();
 }
@@ -8,5 +9,31 @@ const hideStartScreen = () => {
     let selectionScreen = document.getElementById("selectionScreen")
     startScreen.style.display = 'none';
     selectionScreen.style.display = 'block';
-    audioPlay();
+    startAudioPlay();
 }
+
+// Selection Screen
+
+class Character1 {
+    constructor(playerName) {
+        this.playerName = playerName;
+        this.characterName = 'Catto';
+        this.health = 100;
+        this.dmg = 20;
+    }
+
+    attack() {
+        return this.dmg;
+    }
+
+    meow() {
+        return this.health + 10;
+    }
+}
+
+const choosePlayer1Character1 = () => {
+    let playerName = document.getElementById("player1Name").value;
+    let characterName = document.getElementById("character1Button").innerHTML;
+    return new characterName(playerName);
+}
+
