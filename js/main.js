8389c2c1b535e9efdba0bbd5e5b7cf0ac160fe1b
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-05 16:22:58 +0100

    created all characters and assigned them to buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,53 @@
+// Character classes
+class Character {
+    constructor(playerName) {
+        this.characterName = 'Catto';
+        this.playerName = playerName || 'Teapot 418';
+        this.health = 100;
+    }
+    attack() {return this.damage}
+    meow() {return this.defense += 15}
+}
+
+class Catto extends Character {
+    constructor(playerName) {
+        super(playerName);
+        this.damage = 20;
+        this.defense = 50;
+    }
+}
+
+class Clawsy extends Character {
+    constructor(playerName) {
+        super(playerName);
+        this.damage = 35;
+        this.defense = 10;
+    }
+}
+
+class MeowTank extends Character {
+    constructor(playerName) {
+        super(playerName);
+        this.damage = 20;
+        this.defense = 150;
+    }
+}
+
+class NinjaMeow extends Character {
+    constructor(playerName) {
+        super(playerName);
+        this.damage = 10;
+        this.defense = 10;
+    }
+    attack() {return this.damage + getRandomInt(10, 60)}
+    meow() {return this.defense += getRandomInt(10, 30)}
+}
+
+// USEFUL FUNCTIONS
+// getRandomInt to get a random integer between the min and max values
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+
 // Start Screen
 const startAudioPlay = () => {
     let audio = document.getElementById("audio");
@@ -6,7 +56,7 @@ const startAudioPlay = () => {
 
 const hideStartScreen = () => {
     let startScreen = document.getElementById("startScreen");
-    let selectionScreen = document.getElementById("selectionScreen")
+    let selectionScreen = document.getElementById("selectionScreen");
     startScreen.style.display = 'none';
     selectionScreen.style.display = 'block';
 }
@@ -17,26 +67,7 @@ const startToSelectionScreen = () => {
 }
 
 // Selection Screen
-class Character {
-    constructor(characterName, playerName) {
-        this.characterName = characterName;
-        this.playerName = playerName || 'Player 1';
-        this.health = 100;
-        this.dmg = 20;
-    }
-
-    attack() {
-        return this.dmg;
-    }
-
-    meow() {
-        return this.health + 10;
-    }
-}
-
-const player1ChooseCharacter = (characterName) => {
+const chooseCharacter = (characterName) => {
     let playerName = document.getElementById("player1Name").value;
-    return new Character(characterName, playerName);
+    return new characterName(playerName);
 }
-
-console.log(player1ChooseCharacter());
